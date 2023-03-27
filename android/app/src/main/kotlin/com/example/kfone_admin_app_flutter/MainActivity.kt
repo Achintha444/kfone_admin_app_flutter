05402ed7e1e7469ee1ae9785eae0e9d02cbb94d1
Achintha Isuru <achinthaisuru.17@cse.mrt.ac.lk>
2023-03-27 11:38:59 +0530

    init commit

---
+++ android/app/src/main/kotlin/com/example/kfone_admin_app_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.kfone_admin_app_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/theme/theme.dart
@@ -0,0 +1,29 @@
+import "package:flutter/material.dart";
+
+/// Theme data for the project
+ThemeData themeData = ThemeData(
+  primarySwatch: Colors.red,
+  elevatedButtonTheme: ElevatedButtonThemeData(
+    style: ButtonStyle(
+      backgroundColor: MaterialStateProperty.all<Color>(Colors.redAccent),
+      foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
+      textStyle: MaterialStateProperty.all<TextStyle>(
+        const TextStyle(
+          fontSize: 14,
+          letterSpacing: 1,
+          fontWeight: FontWeight.w500,
+        ),
+      ),
+    ),
+  ),
+  appBarTheme: const AppBarTheme(
+    color: Colors.white70,
+    foregroundColor: Colors.black87,
+    titleTextStyle: TextStyle(
+      fontWeight: FontWeight.w700,
+      color: Colors.black87,
+      fontSize: 18,
+    ),
+    
+  ),
+);
