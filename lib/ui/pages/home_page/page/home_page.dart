face79700bca4df0c1aeb50160fe7d8d0221c5d4
Achintha Isuru <achinthaisuru.17@cse.mrt.ac.lk>
2023-03-28 10:43:43 +0530

    home page bloc created

--- lib/ui/pages/home_page/page/home_page.dart
+++ lib/ui/pages/home_page/page/home_page.dart
@@ -1,23 +1,40 @@
 import "package:flutter/material.dart";
 import 'package:kfone_admin_app_flutter/ui/pages/home_page/features/consumers_page/widgets/table_header_widget.dart';
 import 'package:kfone_admin_app_flutter/ui/pages/home_page/features/consumers_page/widgets/tier_chip.dart';
-import 'package:kfone_admin_app_flutter/ui/pages/home_page/features/sales_trend_page/page/sales_trend_page.dart';
+import 'package:kfone_admin_app_flutter/ui/pages/home_page/models/drawer_item.dart';
 import 'package:kfone_admin_app_flutter/ui/pages/home_page/page/home_page_arguements.dart';
-import 'package:kfone_admin_app_flutter/util/common.dart';
 import 'package:kfone_admin_app_flutter/util/model/session_token.dart';
 import 'package:kfone_admin_app_flutter/util/ui_util.dart';
 
-import '../../../widgets/common/resizable_image.dart';
 import '../../account_page/page/account_page.dart';
 import '../../account_page/page/account_page_arguments.dart';
 import '../features/consumers_page/widgets/table_row_data.dart';
-import '../features/sales_trend_page/widgets/bar_graph.dart';
-import '../features/sales_trend_page/widgets/curve_graph.dart';
-import '../features/sales_trend_page/widgets/line_graph.dart';
-import '../features/sales_trend_page/widgets/pie_chart.dart';
 
 class HomePage extends StatelessWidget {
-  static const routeName = "/home";
+  static const String routeName = "/home";
+
+  static const List<DrawerItem> _drawerItems = [
+    DrawerItem(
+      title: 'Devices',
+      icon: Icons.devices_rounded,
+      route: "",
+    ),
+    DrawerItem(
+      title: 'Customers',
+      icon: Icons.devices_rounded,
+      route: "",
+    ),
+    DrawerItem(
+      title: 'Promotions',
+      icon: Icons.devices_rounded,
+      route: "",
+    ),
+    DrawerItem(
+      title: 'Sales Trends',
+      icon: Icons.devices_rounded,
+      route: "",
+    ),
+  ];
 
   const HomePage({super.key});
 
@@ -36,65 +53,111 @@ class HomePage extends StatelessWidget {
         title: const Text("Sales Trend"),
       ),
       drawer: Drawer(
-        child: ListView(
+        child: ListView.builder(
           padding: const EdgeInsets.all(0),
-          children: [
-            DrawerHeader(
-              decoration:
-                  const BoxDecoration(color: Colors.redAccent), //BoxDecoration
-              child: UserAccountsDrawerHeader(
-                decoration: const BoxDecoration(color: Colors.redAccent),
-                accountName: const Text("Abhishek Mishra"),
-                accountEmail: const Text("abhishekm977@gmail.com"),
-                currentAccountPictureSize: const Size.square(50),
-                currentAccountPicture: const Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: CircleAvatar(
-                    backgroundColor: Colors.white70,
-                    child: Text("A"), //Text
+          itemCount: _drawerItems.length + 1,
+          itemBuilder: (BuildContext context, int index) {
+            if (index == 0) {
+              return DrawerHeader(
+                decoration: const BoxDecoration(
+                    color: Colors.redAccent), //BoxDecoration
+                child: UserAccountsDrawerHeader(
+                  decoration: const BoxDecoration(color: Colors.redAccent),
+                  accountName: const Text("Abhishek Mishra"),
+                  accountEmail: const Text("abhishekm977@gmail.com"),
+                  currentAccountPictureSize: const Size.square(50),
+                  currentAccountPicture: const Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: CircleAvatar(
+                      backgroundColor: Colors.white70,
+                      child: Text("A"), //Text
+                    ),
                   ),
-                ),
-                onDetailsPressed: () =>
-                    _onUserDetailsTap(context, args.sessionToken),
-                arrowColor: Colors.white70, //circleAvatar
-              ), //UserAccountDrawerHeader
-            ), //DrawerHeader
-            ListTile(
-              leading: const Icon(Icons.devices_rounded),
-              title: const Text('Devices'),
+                  onDetailsPressed: () =>
+                      _onUserDetailsTap(context, args.sessionToken),
+                  arrowColor: Colors.white70, //circleAvatar
+                ), //UserAccountDrawerHeader
+              );
+            }
+
+            DrawerItem item = _drawerItems[index - 1];
+            return ListTile(
+              leading: Icon(item.icon),
+              title: Text(item.title),
               onTap: () {
                 Navigator.pop(context);
               },
-            ),
-            ListTile(
-              leading: const Icon(Icons.discount_rounded),
-              title: const Text('Promotions'),
-              onTap: () {
-                Navigator.pop(context);
-              },
-            ),
-            ListTile(
-              leading: const Icon(Icons.people_alt_rounded),
-              title: const Text('Customers'),
-              onTap: () {
-                Navigator.pop(context);
-              },
-            ),
-            ListTile(
-              leading: const Icon(Icons.trending_up_rounded),
-              title: const Text('Sales Trends'),
-              onTap: () {
-                Navigator.pop(context);
-              },
-            ),
-            SizedBox(height: MediaQuery.of(context).size.height / 2.6),
-            const ResizableImage(
-              fit: BoxFit.fitHeight,
-              imageLocation: 'assets/images/logo.png',
-              height: 30.0,
-            )
-          ],
+            );
+          },
         ),
+        // child: ListView(
+        //   padding: const EdgeInsets.all(0),
+        //   children: [
+        //     DrawerHeader(
+        //       decoration:
+        //           const BoxDecoration(color: Colors.redAccent), //BoxDecoration
+        //       child: UserAccountsDrawerHeader(
+        //         decoration: const BoxDecoration(color: Colors.redAccent),
+        //         accountName: const Text("Abhishek Mishra"),
+        //         accountEmail: const Text("abhishekm977@gmail.com"),
+        //         currentAccountPictureSize: const Size.square(50),
+        //         currentAccountPicture: const Padding(
+        //           padding: EdgeInsets.all(8.0),
+        //           child: CircleAvatar(
+        //             backgroundColor: Colors.white70,
+        //             child: Text("A"), //Text
+        //           ),
+        //         ),
+        //         onDetailsPressed: () =>
+        //             _onUserDetailsTap(context, args.sessionToken),
+        //         arrowColor: Colors.white70, //circleAvatar
+        //       ), //UserAccountDrawerHeader
+        //     ), //DrawerHeader
+        //     ListTile(
+        //       leading: const Icon(Icons.devices_rounded),
+        //       title: const Text('Devices'),
+        //       onTap: () {
+        //         Navigator.pop(context);
+        //       },
+        //     ),
+        //     ListView.builder(
+        //       itemCount: _drawerItems.length,
+        //       itemBuilder: (BuildContext context, int index) {
+        //         DrawerItem item = _drawerItems[index];
+        //         return new ListTile(
+        //           title: new Text(item.title),
+        //         );
+        //       },
+        //     ),
+        //     ListTile(
+        //       leading: const Icon(Icons.discount_rounded),
+        //       title: const Text('Promotions'),
+        //       onTap: () {
+        //         Navigator.pop(context);
+        //       },
+        //     ),
+        //     ListTile(
+        //       leading: const Icon(Icons.people_alt_rounded),
+        //       title: const Text('Customers'),
+        //       onTap: () {
+        //         Navigator.pop(context);
+        //       },
+        //     ),
+        //     ListTile(
+        //       leading: const Icon(Icons.trending_up_rounded),
+        //       title: const Text('Sales Trends'),
+        //       onTap: () {
+        //         Navigator.pop(context);
+        //       },
+        //     ),
+        //     SizedBox(height: MediaQuery.of(context).size.height / 2.6),
+        //     const ResizableImage(
+        //       fit: BoxFit.fitHeight,
+        //       imageLocation: 'assets/images/logo.png',
+        //       height: 30.0,
+        //     )
+        //   ],
+        // ),
       ),
       body: SizedBox(
         width: UiUtil.getMediaQueryWidth(context),
